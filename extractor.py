b85ce47ba11f594841ea96715fd6afc90e76ad0b
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-18 17:14:24 -0500

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -3,20 +3,22 @@ from img2table.ocr import TesseractOCR
 from PIL import Image as Grayscale
 from PIL import ImageEnhance
 
+
 def extract_price_list(img_path):
-  img = Grayscale.open(img_path).convert("L")
-  sharpness = ImageEnhance.Sharpness(img)
-  img = sharpness.enhance(2.0)
-  img.save('greyscale.png')
-  g_img_path = "greyscale.png"
-  img = Image(src=g_img_path)
-  tesseract = TesseractOCR()
-  tables = img.extract_tables(ocr=tesseract, implicit_rows=False, min_confidence=0)
-  table_work = tables[0].df
-  table_work = table_work.iloc[:, 1]
-  table_work = table_work.tolist()
-  new_table_work = [input_string.replace(',', '').replace('.', '').replace("USD", "").strip() for input_string in table_work]
-  if len(new_table_work) > 10:
+    img = Grayscale.open(img_path).convert("L")
+    sharpness = ImageEnhance.Sharpness(img)
+    img = sharpness.enhance(2.0)
+    img.save('greyscale.png')
+    g_img_path = "greyscale.png"
+    img = Image(src=g_img_path)
+    tesseract = TesseractOCR()
+    tables = img.extract_tables(
+        ocr=tesseract, borderless_tables=False, implicit_rows=True, min_confidence=0)
+    table_work = tables[0].df
+    table_work = table_work.iloc[:, 1]
+    table_work = table_work.tolist()
+    new_table_work = [input_string.replace(',', '').replace(
+        '.', '').replace("USD", "").strip() for input_string in table_work]
+    if len(new_table_work) > 10:
         new_table_work = new_table_work[-10:-1]
-  return new_table_work
-  
+    return new_table_work
