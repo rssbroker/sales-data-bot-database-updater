a3e67ada327c025cebc52ccb21e36ac2dd21835b
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-18 20:38:05 -0500

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -16,8 +16,7 @@ def extract_price_list(img_path):
     table_work = tables[0].df
     table_work = table_work.iloc[:, 1]
     table_work = table_work.tolist()
-    new_table_work = [input_string.replace(',', '').replace(
-        '.', '').replace("USD", "").strip() for input_string in table_work]
+    new_table_work = [input_string.replace(',', '').replace('.', '').replace("USD", "").strip() for input_string in table_work]
     if len(new_table_work) > 10:
         new_table_work = new_table_work[-10:-1]
     return new_table_work
