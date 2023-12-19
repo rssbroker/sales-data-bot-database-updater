e43e5b68da12c5c1a959d9751be07e58a34e23fb
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-19 08:23:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ password = os.environ["NAMEBIO_PASSWORD"]
 website_url = "https://namebio.com"
 img_path = "table.png"
 
+def update_counter():
+    r.set("counter", (r.get("counter") + 1) % 24)
 
 def get_data_from_website(page_source):
     soup = BeautifulSoup(page_source, 'html.parser')
@@ -48,8 +50,11 @@ def get_data_from_website(page_source):
 
 
 def set_database_records():
-    records_list = get_data_from_website(get_html_page())
-
+    page = get_html_page()
+    records_list1 = get_data_from_website(page[0])
+    records_list2 = get_data_from_website(page[1])
+    records_list3 = get_data_from_website(pagep[2])
+    records_list = records_list1 + records_list2 +records_list3
     prices_b = [x["Price"] for x in records_list]
     prices_a = extractor.extract_price_list(img_path)
     prices_b = list_corrector.restore_strings(prices_a, prices_b)
@@ -67,7 +72,9 @@ def set_database_records():
 
 
 def get_html_page():
-    received_html = ''
+    received_html1 = ''
+    received_html2 = ''
+    received_html3 = ''
     chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     chrome_options = Options()
     options = ["--headless", "--window-size=6000x5000", "--no-sandbox", "--force-device-scale-factor=4.0"]
@@ -105,53 +112,59 @@ def get_html_page():
         time.sleep(10)
 
         # Get the URL after login
-        received_html = driver.page_source
+        received_html1 = driver.page_source
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table1.png")
         button_next = driver.find_element(By.XPATH, '//*[@id="search-results_paginate"]/ul/li[3]/a')
         button_next.click()
         time.sleep(3)
+        received_html2 = driver.page_source
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table2.png")
         button_next = driver.find_element(By.XPATH, '//*[@id="search-results_paginate"]/ul/li[4]/a')
         button_next.click()
         time.sleep(3)
+        received_html3 = driver.page_source
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table3.png")
 
     except NoSuchElementException:
-        received_html = driver.page_source
+        received_html1 = driver.page_source
         time.sleep(10)
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table1.png")
         button_next = driver.find_element(By.XPATH, '//*[@id="search-results_paginate"]/ul/li[3]/a')
         button_next.click()
         time.sleep(3)
+        received_html2 = driver.page_source
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table2.png")
         button_next = driver.find_element(By.XPATH, '//*[@id="search-results_paginate"]/ul/li[4]/a')
         button_next.click()
         time.sleep(3)
+        received_html3 = driver.page_source
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table3.png")
 
     finally:
-        received_html = driver.page_source
+        received_html1 = driver.page_source
         time.sleep(10)
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table1.png")
         button_next = driver.find_element(By.XPATH, '//*[@id="search-results_paginate"]/ul/li[3]/a')
         button_next.click()
         time.sleep(3)
+        received_html2 = driver.page_source
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table2.png")
         button_next = driver.find_element(By.XPATH, '//*[@id="search-results_paginate"]/ul/li[4]/a')
         button_next.click()
         time.sleep(3)
+        received_html3 = driver.page_source
         table_img = driver.find_element(By.ID, "search-results")
         table_img.screenshot("table3.png")
         driver.quit()
-    return received_html
+    return received_html1, received_html2, received_html3
 
 
 if __name__ == "__main__":
