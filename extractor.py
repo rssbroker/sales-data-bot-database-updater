7115d74fa665973d4ee5eda8dd4c308764dd147e
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-18 19:30:21 -0500

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -13,7 +13,7 @@ def extract_price_list(img_path):
     img = Image(src=g_img_path)
     tesseract = TesseractOCR()
     tables = img.extract_tables(
-        ocr=tesseract, borderless_tables=False, implicit_rows=True, min_confidence=0)
+        ocr=tesseract, min_confidence=0)
     table_work = tables[0].df
     table_work = table_work.iloc[:, 1]
     table_work = table_work.tolist()
