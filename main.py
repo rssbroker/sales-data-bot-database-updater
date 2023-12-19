920633218cf458454c2c0187acbe95bcbaf97d51
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-18 22:22:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def get_html_page():
     received_html = ''
     chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     chrome_options = Options()
-    options = ["--headless", "--disable-gpu", "--window-size=6000x5000", "--ignore-certificate-errors", "--disable-extensions", "--no-sandbox", "--force-device-scale-factor={4.0}"]
+    options = ["--headless", "--window-size=6000x5000", "--ignore-certificate-errors", "--disable-extensions", "--no-sandbox", "--force-device-scale-factor={4.0}"]
     
     for option in options:
         chrome_options.add_argument(option)
