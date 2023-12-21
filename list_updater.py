5965c527d442ec9fb057b75ceb0992d13828a467
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-21 13:53:17 -0500

    Update list_updater.py

--- list_updater.py
+++ list_updater.py
@@ -6,15 +6,15 @@ def update(list_a, list_b, counter):
     
   list_a_new = find_unique_elements(list_a, list_b)
   index = 0
-  if len(list_a_new) <= (10 - counter - 1):
+  if len(list_a_new) <= (30 - counter - 1):
     for i in range(counter, counter + len(list_a_new)):
       list_b[i] = list_a_new[index]
       index = index + 1
   else:
-    for i in range(counter, 10):
+    for i in range(counter, 30):
       list_b[i] = list_a_new[index]
       index = index + 1
-    for j in range(len(list_a_new) - 10 + counter):
+    for j in range(len(list_a_new) - 30 + counter):
       list_b[i] = list_a_new[index]
       index = index + 1
       
