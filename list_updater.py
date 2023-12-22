65e90e53bc7ec0aef32380ef9ae63a159af52a0d
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-22 04:53:08 -0500

    Update list_updater.py

--- list_updater.py
+++ list_updater.py
@@ -1,36 +1,5 @@
-def update(list_a, list_b, counter):
-  def find_unique_elements(list_a, list_b):
+def find_unique_elements(list_a, list_b):
     set_b = set(map(lambda d: frozenset(d.items()), list_b))
-    list_a_new = [item for item in list_a if frozenset(item.items()) not in set_b]
+    list_a_new = [item for item in list_a if frozenset(
+        item.items()) not in set_b]
     return list_a_new
-
-  def get_sublist(list_a, start_index):
-    start_index = start_index % len(list_a)
-    sublist = list_a[start_index:] + list_a[:start_index]
-    return sublist
-
-  def insert_lists_into_empty_list(list1, list2, start_index):
-    empty_list = [0] * 30
-    size = len(empty_list)
-
-    i = 0
-    while i < size and (list1 or list2):
-      # Calculate the current index for insertion, looping if needed
-      current_index = (start_index + i) % size
-
-      # Check if list1 has elements to insert
-      if list1:
-        empty_list[current_index] = list1.pop(0)
-        # Check if list2 has elements to insert after list1 is exhausted
-      elif list2:
-        empty_list[current_index] = list2.pop(0)
-
-      i += 1
-
-    return empty_list
-
-  list_a_new = find_unique_elements(list_a, list_b)
-  list_b_corrected = get_sublist(list_b, counter)
-  new_list_b = insert_lists_into_empty_list(list_a_new, list_b_corrected, counter)
-
-  return new_list_b
