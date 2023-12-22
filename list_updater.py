95e2b0e9d0fb1d92434cc8ef3c70ca16d7f36804
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-21 20:09:21 -0500

    Update list_updater.py

--- list_updater.py
+++ list_updater.py
@@ -3,30 +3,34 @@ def update(list_a, list_b, counter):
     set_b = set(map(lambda d: frozenset(d.items()), list_b))
     list_a_new = [item for item in list_a if frozenset(item.items()) not in set_b]
     return list_a_new
-    
-  list_a_new = find_unique_elements(list_a, list_b)
 
-  unordered_counter = counter
-  while unordered_counter < len(list_b) and list_b[unordered_counter] in list_a:
-    unordered_counter += 1
-  
-  index = 0
-  if len(list_a_new) <= (30 - unordered_counter - 1):
-    for i in range(unordered_counter, unordered_counter + len(list_a_new)):
-      list_b[i] = list_a_new[index]
-      index = index + 1
-      if (index == len(list_a_new)):
-        break
-  else:
-    for i in range(unordered_counter, 30):
-      list_b[i] = list_a_new[index]
-      index = index + 1
-      if (index == len(list_a_new)):
-        break
-    for j in range(len(list_a_new) - 30 + unordered_counter):
-      list_b[j] = list_a_new[index]
-      index = index + 1
-      if (index == len(list_a_new)):
-        break
-      
-  return list_b
+  def get_sublist(list_a, start_index):
+    start_index = start_index % len(list_a)
+    sublist = list_a[start_index:] + list_a[:start_index]
+    return sublist
+
+  def insert_lists_into_empty_list(list1, list2, start_index):
+    empty_list = [0] * 30
+    size = len(empty_list)
+
+    i = 0
+    while i < size and (list1 or list2):
+      # Calculate the current index for insertion, looping if needed
+      current_index = (start_index + i) % size
+
+      # Check if list1 has elements to insert
+      if list1:
+        empty_list[current_index] = list1.pop(0)
+        # Check if list2 has elements to insert after list1 is exhausted
+      elif list2:
+        empty_list[current_index] = list2.pop(0)
+
+      i += 1
+
+    return empty_list
+
+  list_a_new = find_unique_elements(list_a, list_b)
+  list_b_corrected = get_sublist(list_b, counter)
+  new_list_b = insert_lists_into_empty_list(list_a_new, list_b_corrected, counter)
+
+  return new_list_b
