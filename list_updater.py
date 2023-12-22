b0328bc0e1706928ff54be8ba3b275c3a3b68d51
rssbroker <153944461+rssbroker@users.noreply.github.com>
2023-12-22 04:53:33 -0500

    Update list_updater.py

--- list_updater.py
+++ list_updater.py
@@ -1,5 +1,4 @@
 def find_unique_elements(list_a, list_b):
     set_b = set(map(lambda d: frozenset(d.items()), list_b))
-    list_a_new = [item for item in list_a if frozenset(
-        item.items()) not in set_b]
+    list_a_new = [item for item in list_a if frozenset(item.items()) not in set_b]
     return list_a_new
